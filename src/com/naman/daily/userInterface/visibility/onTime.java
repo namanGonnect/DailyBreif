e7ddc741718ab389a7348e33ad01d0ef57e974fc
Naman Malhotra <malhotranaman11@gmail.com>
2020-07-29 16:32:13 +0200

    New visibility requirement.

---
+++ src/com/naman/daily/userInterface/visibility/onTime.java
@@ -0,0 +1,8 @@
+package com.naman.daily.userInterface.visibility;
+import com.naman.daily.userInterface.frame.windowInterface;
+
+public class onTime {
+    public static void visibilityOnTime(windowInterface ui) {
+        ui.getTextAreaMain().setText(null);
+    }
+}
